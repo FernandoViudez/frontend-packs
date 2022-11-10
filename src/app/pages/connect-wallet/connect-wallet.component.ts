b71e19e98493c9baff3671be783162bcd7781470
JesusOyola <jesus.oyola@interatica.com>
2022-11-10 10:37:55 -0300

    wallet conect implement

--- src/app/pages/connect-wallet/connect-wallet.component.ts
+++ src/app/pages/connect-wallet/connect-wallet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PeraWalletConnect } from '@perawallet/connect';
 
 @Component({
   selector: 'app-connect-wallet',
@@ -12,4 +13,29 @@ export class ConnectWalletComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  title = 'perawallet';
+  peraWallet = new PeraWalletConnect({
+    shouldShowSignTxnToast: false,
+  });
+  connectButton: any = document.querySelector('#titile');
+  accountAddress: string = '';
+
+  handleConnectWalletClick() {
+    this.peraWallet
+      .connect()
+      .then((newAccounts) => {
+        this.peraWallet.connector?.on('disconnect', this.handleDisconnectWalletClick);
+
+        console.log(newAccounts[0]);
+      })
+      .catch((error) => {
+        if (error?.data?.type !== 'CONNECT_MODAL_CLOSED') {
+        }
+      });
+  }
+
+  handleDisconnectWalletClick() {
+    this.peraWallet.disconnect();
+  }
 }
