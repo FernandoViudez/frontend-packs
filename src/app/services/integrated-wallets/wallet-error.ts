f5c79dc9213fbdef866d9eb7967e6549596f6618
ElianArismendi <elian.arismendi@interatica.com>
2022-11-14 12:31:27 -0300

    implement error handler for all wallet integrations

---
+++ src/app/services/integrated-wallets/wallet-error.ts
@@ -0,0 +1,8 @@
+export const enum WalletErrorCode {
+  connectionMethodNotSupported = 'WALLET_CONNECTION_METHOD_IS_NOT_SUPPORTED',
+  errorTryingToConnect = 'ERROR_TRYING_TO_CONNECT_WALLET',
+}
+export const enum WalletErrorMessage {
+  connectionMethodNotSupported = 'Wallet connection method is not supported.',
+  errorTryingToConnect = 'Error  trying to connect wallet.',
+}
--- src/app/services/wallet-handler.service.ts
+++ src/app/services/wallet-handler.service.ts
@@ -1,11 +1,13 @@
 /* eslint-disable indent */
 import { Injectable } from '@angular/core';
-import { Observable, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { LS_KEY, WalletState } from '../guards/wallet-is-connected/wallet-states';
 import { Account } from '../interfaces/account.interface';
+import { ConnectionAttemptFailed } from '../interfaces/connection-attempt.-failed.interface';
 import { IntegratedWallet } from './integrated-wallets/integrated-wallets';
 import { MyAlgoService } from './integrated-wallets/my-algo.service';
 import { PeraWalletService } from './integrated-wallets/pera-wallet.service';
+import { WalletErrorCode, WalletErrorMessage } from './integrated-wallets/wallet-error';
 
 @Injectable({
   providedIn: 'root',
@@ -15,17 +17,37 @@ export class WalletHandlerService {
 
   constructor(private myAlgo: MyAlgoService, private readonly peraWallet: PeraWalletService) {}
 
-  connectWalletByIntegrationSelected(wallet: IntegratedWallet): Observable<Account> {
-    switch (wallet) {
-      // eslint-disable-next-line indent
-      case IntegratedWallet.myAlgo:
-        return this.myAlgo.connectWallet().pipe(tap(this.saveAccountInfo.bind(this)));
-      case IntegratedWallet.peraWallet:
-        return this.peraWallet.connectWallet().pipe(tap(this.saveAccountInfo.bind(this)));
-      default:
-        localStorage.setItem(LS_KEY, WalletState.error);
-        return throwError(() => new Error('INTEGRATION_NOT_IMPLEMENTED'));
-    }
+  connectWalletByIntegrationSelected(
+    wallet: IntegratedWallet
+  ): Observable<Account | ConnectionAttemptFailed> {
+    if (!this[wallet]) return this.handleWalletNotSupportedException();
+    return this.handleWalletConnection(this[wallet].connectWallet());
+  }
+
+  private handleWalletNotSupportedException(): Observable<Account | ConnectionAttemptFailed> {
+    localStorage.setItem(LS_KEY, WalletState.error);
+    return of({
+      errorMessage: WalletErrorCode.connectionMethodNotSupported,
+      logs: null,
+    } as ConnectionAttemptFailed);
+  }
+
+  private handleWalletConnection(
+    connectionAttempt: Observable<Account>
+  ): Observable<Account | ConnectionAttemptFailed> {
+    return connectionAttempt.pipe(
+      map((response) => {
+        this.saveAccountInfo(response);
+        return response;
+      }),
+      catchError((error) =>
+        of({
+          errorCode: WalletErrorCode.errorTryingToConnect,
+          errorMessage: error?.message || WalletErrorMessage.errorTryingToConnect,
+          logs: error,
+        } as ConnectionAttemptFailed)
+      )
+    );
   }
 
   private saveAccountInfo(account: Account): void {
